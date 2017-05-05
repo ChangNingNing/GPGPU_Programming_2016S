acc0682d3ad3cebde1a27ff7a469fb58c3013927
ChangNingNing <r05922087@ntu.edu.tw>
2017-05-05 16:20:56 +0800

    local memory version!

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -29,68 +29,48 @@ void CountPosition1(const char *text, int *pos, int text_size)
 }
 
 #define ThreadSize 1024
-/*
-__global__ void myTransform(const char *text, int *pos, int n){
-	int x = blockIdx.x, y = threadIdx.x;
-	int index = x * blockDim.x + y;
 
-	if (index < n){
-		if (text[index] != ' ' && text[index] != '\n')
-			pos[index] = 1;
-		else
-			pos[index] = 0;
-	}
-}
-*/
-
-__global__ void myBuildTree(const char *text, int *pos, int *BIT, int n){
+__global__ void myCudaCount(const char *text, int *pos, int n){
 	int x = blockIdx.x;
-	int y = (blockIdx.y == 1)? threadIdx.x + ThreadSize/2: threadIdx.x;
-	int index = x * blockDim.x + y;
-	int left = (blockIdx.y == 1)? x*blockDim.x + ThreadSize/2: x*blockDim.x;
+	int left = (blockIdx.y == 1)? x * blockDim.x + ThreadSize/2: x * blockDim.x;
+	int y = threadIdx.x;
+	int index = left + y;
+	int mapIndex = index - left;
 
-//	__shared__ int _BIT[ThreadSize][10];
+	__shared__ int BIT[ThreadSize][10];
 
 	if (index < n){
-		/* Transform */
+		// Transform
 		if (text[index] != ' ' && text[index] != '\n')
-			BIT[index] = 1;
+			BIT[mapIndex][0] = 1;
 		else
-			BIT[index] = 0;
+			BIT[mapIndex][0] = 0;
 		__syncthreads();
 
-		/* Build tree */
-		int *row_pre = (int *)(BIT);
-		int *row_cur = (int *)(BIT + n);
+		// Build tree
 		int base = 1;
-		for (int i=1; i<10; i++){
-			int offset = (1 << (i-1));
+		int offset = 1;
+		for (int i=1; i<10; i++, offset <<= 1){
 			int tmp = index - offset;
 			if (tmp >= left){
-				if (row_pre[tmp] != 0 && row_pre[index] != 0){
-					row_cur[index] = row_pre[index] + row_pre[tmp];
+				if (BIT[tmp - left][i-1] != 0 && BIT[mapIndex][i-1] != 0){
+					BIT[mapIndex][i] = BIT[mapIndex][i-1] + BIT[tmp - left][i-1];
 					base = i + 1;
 				}
 				else
-					row_cur[index] = 0;
+					BIT[mapIndex][i] = 0;
 			}
 			else{
-				row_cur[index] = row_pre[index];
+				BIT[mapIndex][i] = BIT[mapIndex][i-1];
 				base = i + 1;
 			}
 			__syncthreads();
-
-			row_pre = row_cur;
-			row_cur += n;
 		}
-		__syncthreads();
 
-		/* Set */
-		int offset = index;
-		row_cur = (int *)(BIT + (base-1)*n);
+		// Set
+		offset = index;
 		for (int i=base-1; i>=0 && offset>=left; i--){
-			offset -= row_cur[offset];
-			row_cur -= n;
+			offset -= BIT[offset - left][i];
 		}
 
 		if (index >= left + ThreadSize/2 || index < ThreadSize / 2)
@@ -98,43 +78,8 @@ __global__ void myBuildTree(const char *text, int *pos, int *BIT, int n){
 	}
 }
 
-/*
-__global__ void mySet(int *pos, int *BIT, int n){
-	int x = blockIdx.x, y = threadIdx.x;
-	int index = x * blockDim.x + y;
-
-	if (index < n){
-		int base = 0;
-		int *row = (int *)(BIT);
-		for (; base < 10; base++){
-			if (row[index] == 0)
-				break;
-			row += n;
-		}
-
-		int offset = index;
-		row -= n;
-		for (int i=base-1; i>=0 && offset>=0; i--){
-			offset -= row[offset];
-			row -= n;
-		}
-
-		pos[index] = index - offset;
-	}
-}
-*/
-
 void CountPosition2(const char *text, int *pos, int text_size)
 {
-	int *BIT;
-	size_t pitch;
-//	dim3 grid(CeilDiv(text_size, ThreadSize), 1), block(ThreadSize, 1);
-	dim3 grid2(CeilDiv(text_size, ThreadSize), 2), block(ThreadSize, 1);
-	cudaMallocPitch(&BIT, &pitch, sizeof(int)*text_size, 10);
-
-//	myTransform<<< grid, block>>>(text, BIT, text_size);
-	myBuildTree<<< grid2, block>>>(text, pos, BIT, text_size);
-//	mySet<<< grid, block>>>(pos, BIT, text_size);
-
-	cudaFree(BIT);
+	dim3 grid(CeilDiv(text_size, ThreadSize), 2), block(ThreadSize, 1);
+	myCudaCount<<< grid, block>>>(text, pos, text_size);
 }
